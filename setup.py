5df2f5e307cdf40be3148585e6a9aed4c1a406d6
Pablo Saavedra <saavedra.pablo@gmail.com>
2016-02-27 13:46:32 +0100

    Fixed several PEP8 complaints

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup, find_packages
 
-version = "0.11.0"
+version = "1.0.0"
 
 long_description = ""
 try:
-    long_description=file('README.md').read()
+    long_description = file('README.md').read()
 except Exception:
     pass
 
 license = ""
 try:
-    license=file('LICENSE').read()
+    license = file('LICENSE').read()
 except Exception:
     pass
 
 
 setup(
-    name = 'redmine-cmd',
-    version = version,
-    description = 'redmine-cmd.cfg.example',
-    author = 'Pablo Saavedra',
-    author_email = 'saavedra.pablo@gmail.com',
-    url = 'http://github.com/psaavedra/redmine-cmd',
-    packages = find_packages(),
+    name='redmine-cmd',
+    version=version,
+    description='redmine-cmd.cfg.example',
+    author='Pablo Saavedra',
+    author_email='saavedra.pablo@gmail.com',
+    url='http://github.com/psaavedra/redmine-cmd',
+    packages=find_packages(),
     package_data={
     },
     scripts=[
@@ -37,12 +37,12 @@ setup(
     ],
     data_files=[
         ('/usr/share/doc/redmine-cmd/', ['cfg/redmine_cmd.cfg.example']),
-    #     ('/etc/init.d', ['init-script'])
+        #     ('/etc/init.d', ['init-script'])
     ],
 
-    download_url= 'https://github.com/psaavedra/redmine-cmd/zipball/master',
+    download_url='https://github.com/psaavedra/redmine-cmd/zipball/master',
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 2 - Beta",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
@@ -51,5 +51,5 @@ setup(
     ],
     long_description=long_description,
     license=license,
-    keywords = "python redmine cmd tracker",
+    keywords="python redmine cmd tracker",
 )
